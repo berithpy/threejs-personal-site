44ab2547018c789082aab724339ee7dae953ac93
Diego Diaz <berith.py@gmail.com>
2021-07-30 00:16:39 -0400

    Add more models, lighting for about section

--- main.js
+++ main.js
@@ -44,53 +44,82 @@ const pointLight = new THREE.PointLight(0xffffff);
 
 pointLight.position.set(5, 5, 5);
 
+// aboutPointLight 
+const aboutPointLight = new THREE.PointLight(0xffff44, 1, 10);
+
+aboutPointLight.position.set(1, 1, 9);
 // ambient light
 const ambientLight = new THREE.AmbientLight(0xaaaaaa);
 
-scene.add(pointLight, ambientLight);
+scene.add(pointLight, ambientLight, aboutPointLight);
 
 // point light helper
-// const sphereSize = 1;
-// const lightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper);
+const sphereSize = 1;
+const lightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+const lightHelper2 = new THREE.PointLightHelper(aboutPointLight, sphereSize);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper, lightHelper2);
 
 // const controls = new OrbitControls(camera, renderer.domElement);
 // Loader
 const loader = new GLTFLoader();
 
-loader.load(
-  "models/office.glb",
-  function (gltf) {
-    gltf.scene.position.z = -1;
-    gltf.scene.position.y = -1;
-    gltf.scene.position.x = -0.5;
-    gltf.scene.rotation.y = -1;
-    gltf.scene.rotation.x = 0.15;
-    scene.add(gltf.scene);
-  },
-  undefined,
-  function (error) {
-    console.error(error);
-  }
-);
-var monitor;
-loader.load(
-  "models/monitor.glb",
-  function (glmonitor) {
-    const t = document.body.getBoundingClientRect().top;
-    monitor = glmonitor;
-    monitor.scene.position.x = 0.9;
-    monitor.scene.position.z = 8;
-    monitor.scene.position.y = 0.1;
-    monitor.scene.rotation.y = (t / 893) * 2 - 1;
-    scene.add(monitor.scene);
-  },
-  undefined,
-  function (error) {
-    console.error(error);
-  }
-);
+function loadModel(url) {
+  return new Promise((resolve) => {
+    loader.load(url, resolve);
+  });
+}
+
+let cart1,
+  cart2,
+  cart3,
+  cart4,
+  cart5,
+  cart6,
+  cart7,
+  cart8,
+  monitor,
+  office,
+  gamepad,
+  keyboard;
+
+let p1 = loadModel("models/cart1.glb").then((result) => {
+  cart1 = result.scene.children[0];
+});
+let p2 = loadModel("models/cart2.glb").then((result) => {
+  cart2 = result.scene.children[0];
+});
+let p3 = loadModel("models/cart3.glb").then((result) => {
+  cart3 = result.scene.children[0];
+});
+let p4 = loadModel("models/cart4.glb").then((result) => {
+  cart4 = result.scene.children[0];
+});
+let p5 = loadModel("models/cart5.glb").then((result) => {
+  cart5 = result.scene.children[0];
+});
+let p6 = loadModel("models/cart6.glb").then((result) => {
+  cart6 = result.scene.children[0];
+});
+let p7 = loadModel("models/cart7.glb").then((result) => {
+  cart7 = result.scene.children[0];
+});
+let p8 = loadModel("models/cart8.glb").then((result) => {
+  cart8 = result.scene.children[0];
+});
+let pmonitor = loadModel("models/monitor.glb").then((result) => {
+  monitor = result.scene;
+});
+let poffice = loadModel("models/office.glb").then((result) => {
+  office = result.scene;
+});
+let pgamepad = loadModel("models/gamepad.glb").then((result) => {
+  gamepad = result.scene.children[0];
+});
+let pkeyboard = loadModel("models/keyboard.glb").then((result) => {
+  keyboard = result.scene.children[0];
+});
+
 // Background
 const spaceTexture = new THREE.TextureLoader().load("grid.jpg");
 scene.background = spaceTexture;
@@ -135,7 +164,7 @@ function addStar() {
 
   star.position.set(x, y, z);
   star.rotation.set(r, r, r);
-  scene.add(star);
+  // scene.add(star);
 }
 
 Array(300).fill().forEach(addStar);
@@ -154,15 +183,20 @@ function moveCamera() {
   camera.position.x = t * -0.0002;
   camera.position.y = t * -0.0002;
   if (monitor) {
-    monitor.scene.rotation.y = (t / 893) * 2 - 1;
+    monitor.rotation.y = (t / 893) * 2;
   }
 }
 document.body.onscroll = moveCamera;
 moveCamera();
 
 //Update loop
+function renderLoop() {
+  requestAnimationFrame(renderLoop);
+  animate();
+  renderer.render(scene, camera);
+  // console.log(document.body.getBoundingClientRect().top);
+}
 function animate() {
-  requestAnimationFrame(animate);
   // this rotation can probably be some kind of function
   torus.rotation.x += 0.0014;
   torus.rotation.y += 0.001;
@@ -170,9 +204,61 @@ function animate() {
   torus2.rotation.x -= 0.001;
   torus2.rotation.y -= 0.0013;
   torus2.rotation.z -= 0.0017;
-  // monitor.scene.rotation.y -= 0.01;
-  renderer.render(scene, camera);
-  // console.log(document.body.getBoundingClientRect().top);
 }
+// This is to start the page only after the models are loaded
+Promise.all([
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  pmonitor,
+  poffice,
+  pgamepad,
+  pkeyboard,
+]).then(() => {
+  const t = document.body.getBoundingClientRect().top;
+  //Office objects
+  office.position.set(-0.5, -1, -1);
+  office.rotation.set(0.15, -1, 0);
 
-animate();
+  //do something to the model
+  cart1.position.set(0, 1, 0);
+  cart2.position.set(0, 1, 0);
+  cart3.position.set(0, 1, 0);
+  cart4.position.set(0, 1, 0);
+  cart5.position.set(0, 1, 0);
+  cart6.position.set(0, 1, 0);
+  cart7.position.set(0, 1, 0);
+  cart8.position.set(0, 1, 0);
+  //About me objects
+  monitor.position.set(0.3, -0.4, 9);
+  monitor.rotation.y = (t / 893) * 2;
+  keyboard.position.set(0.7, 0.3, 9);
+  keyboard.rotation.set(1.3, -0.2, 0);
+
+  gamepad.position.set(0, 1, 0);
+  gamepad.rotation.set(1, 0, 0);
+
+  //add model to the scene
+  scene.add(
+    cart1,
+    cart2,
+    cart3,
+    cart4,
+    cart5,
+    cart6,
+    cart7,
+    cart8,
+    monitor,
+    office,
+    keyboard,
+    gamepad
+  );
+
+  //continue the process
+  renderLoop();
+});
