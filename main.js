80d03d68c4819d63cab742ef6000073b6214a3c2
berithpy <berith.py@gmail.com>
2021-08-14 13:44:35 -0400

    moved files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { GenerateTorus } from "./factory"
+import { GenerateTorus, addCart } from "./factory"
 import { getRandomRotation } from "./utils"
 // Camera and Render
 const scene = new THREE.Scene();
@@ -40,17 +40,22 @@ pointLight.position.set(5, 5, 5);
 const aboutPointLight = new THREE.PointLight(0xffff44, 1, 10);
 
 aboutPointLight.position.set(1, 1, 9);
+
+// aboutPointLight 
+const experiencePointLight = new THREE.PointLight(0xffffff, 1, 16);
+
+experiencePointLight.position.set(0.2, 0.3, 26);
 // ambient light
 const ambientLight = new THREE.AmbientLight(0xaaaaaa);
 
-scene.add(pointLight, ambientLight, aboutPointLight);
+scene.add(pointLight, ambientLight, aboutPointLight, experiencePointLight);
 
 // point light helper
-// const sphereSize = 1;
-// const lightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
-// const lightHelper2 = new THREE.PointLightHelper(aboutPointLight, sphereSize);
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper, lightHelper2);
+const sphereSize = 1;
+const lightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+const lightHelper2 = new THREE.PointLightHelper(experiencePointLight, sphereSize);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper, lightHelper2);
 
 // const controls = new OrbitControls(camera, renderer.domElement);
 // Loader
@@ -123,53 +128,25 @@ const me = new THREE.Mesh(
   new THREE.MeshBasicMaterial({ map: meTexture })
 );
 me.position.set(-0.2, 0.2, -1);
-me.rotation.set(0.1, 0.1, 0);
 
 scene.add(me);
 
-// Moon
-const moonTexture = new THREE.TextureLoader().load("moon.jpg");
-const moonNormal = new THREE.TextureLoader().load("normal.jpg");
+// // Moon
+// const moonTexture = new THREE.TextureLoader().load("moon.jpg");
+// const moonNormal = new THREE.TextureLoader().load("normal.jpg");
 
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: moonNormal })
-);
-moon.position.z = 30;
-moon.position.x = -10;
+// const moon = new THREE.Mesh(
+//   new THREE.SphereGeometry(3, 32, 32),
+//   new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: moonNormal })
+// );
+// moon.position.z = 30;
+// moon.position.x = -10;
 
-scene.add(moon);
+// scene.add(moon);
 
-// Stars
-function addStar() {
-  let size = Math.random();
-  const geometry = new THREE.BoxGeometry(size, size, size);
-  const material = new THREE.MeshStandardMaterial({
-    color: 0x51e5ff,
-    wireframe: true,
-  });
-  const star = new THREE.Mesh(geometry, material);
-
-  const [x, y, z, r] = Array(4)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(150));
-
-  star.position.set(x, y, z);
-  star.rotation.set(r, r, r);
-  scene.add(star);
-}
-
-Array(300).fill().forEach(addStar);
 
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
-
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
-
-  me.rotation.x -= 0.01;
-  me.rotation.y -= 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
@@ -179,6 +156,10 @@ function moveCamera() {
   }
 }
 document.body.onscroll = moveCamera;
+
+// This sets the rotation of the moveCamera to the current one 
+// depending on where you are on the scroll bar when you load, 
+// mostly usefull for when figuring out positions
 moveCamera();
 
 // This is to start the page only after the models are loaded
@@ -197,27 +178,64 @@ Promise.all([
   pkeyboard,
 ]).then(() => {
   const t = document.body.getBoundingClientRect().top;
+  //here you can modifiy the model on load
   //Office objects
   office.position.set(-0.5, -1, -1);
   office.rotation.set(0.15, -1, 0);
 
-  //do something to the model
-  cart1.position.set(0, 1, 0);
-  cart2.position.set(0, 1, 0);
-  cart3.position.set(0, 1, 0);
-  cart4.position.set(0, 1, 0);
-  cart5.position.set(0, 1, 0);
-  cart6.position.set(0, 1, 0);
-  cart7.position.set(0, 1, 0);
-  cart8.position.set(0, 1, 0);
+
   //About me objects
   monitor.position.set(0.3, -0.4, 9);
   monitor.rotation.y = (t / 893) * 2;
   keyboard.position.set(0.7, 0.3, 9);
   keyboard.rotation.set(1.3, -0.2, 0);
 
-  gamepad.position.set(0, 1, 0);
+  // These carts can probably be random?
+  //Experience objects
+  //Group 1
+  cart1.position.set(0, 0.3, 15.5);
+  cart1.rotation.set(2, 0, 0);
+  cart2.position.set(0.1, 0.4, 15.6);
+  cart2.rotation.set(2, 0, 1);
+  //Group 2
+  cart4.position.set(0, 0.4, 16.5);
+  cart4.rotation.set(1, 0, 0);
+  cart5.position.set(0.2, 0.3, 16.7);
+  cart5.rotation.set(3, 0, 0);
+  //Group 3
+  const gamepad2 = gamepad.clone()
+  gamepad2.position.set(0, 0.3, 17.5);
+  gamepad2.rotation.set(0, 1.5, 1);
+  //Group 4
+  cart3.position.set(0.2, 0.4, 18.6);
+  cart3.rotation.set(2, 0, 1);
+  gamepad.position.set(0.2, 0.3, 18.5);
   gamepad.rotation.set(1, 0, 0);
+  //Group5
+  cart6.position.set(0.2, 0.4, 19.6);
+  cart6.rotation.set(3, 0, 0);
+  cart7.position.set(0.1, 0.3, 19.6);
+  cart7.rotation.set(3, 1, -0.5);
+  //Group6
+  const cart9 = cart8.clone()
+  cart8.position.set(0.2, 0.4, 20.6);
+  cart8.rotation.set(0, 1, 0);
+  cart9.position.set(0.2, 0.3, 20.5);
+  cart9.rotation.set(1, 0, 2);
+  //Group7
+  const cart10 = cart6.clone()
+  const cart11 = cart7.clone()
+  cart10.position.set(0.2, 0.5, 21.6);
+  cart10.rotation.set(0, 0, 1);
+  cart11.position.set(0.2, 0.4, 21.6);
+  cart11.rotation.set(1, 0, 0);
+  //Group8
+  const cart12 = cart1.clone()
+  const cart13 = cart4.clone()
+  cart12.position.set(0.2, 0.5, 22.6);
+  cart12.rotation.set(1, 0, 1);
+  cart13.position.set(0.2, 0.4, 22.6);
+  cart13.rotation.set(1, 0, 1);
 
   //add model to the scene
   scene.add(
@@ -232,9 +250,27 @@ Promise.all([
     monitor,
     office,
     keyboard,
-    gamepad
+    gamepad,
+    gamepad2,
+    cart9,
+    cart10,
+    cart11,
+    cart12,
+    cart13,
   );
-
+  const starObjects = [
+    cart1,
+    cart2,
+    cart3,
+    cart4,
+    cart5,
+    cart6,
+    cart7,
+    cart8,
+    monitor,
+    gamepad,
+    keyboard]
+  Array(300).fill().forEach(() => { addCart(starObjects, scene) });
   //continue the process
   renderLoop();
 });
@@ -262,6 +298,12 @@ function animate() {
   torus2.rotation.x -= 0.001 * speed;
   torus2.rotation.y -= 0.0013 * speed;
   torus2.rotation.z -= 0.0017 * speed;
+  torus3.rotation.x += 0.00017 * speed;
+  torus3.rotation.y += 0.00017 * speed;
+  torus3.rotation.z += 0.00017 * speed;
+  torus4.rotation.x += 0.000017 * speed;
+  torus4.rotation.y += 0.000017 * speed;
+  torus4.rotation.z += 0.000017 * speed;
   if (speed > 1) {
     speed = speed / 1.01
   }
